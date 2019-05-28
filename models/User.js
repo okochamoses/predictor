4847b9317cbcbe07f05bb30214ef3e2f518c0db3
Moses Okocha <Moses.Okocha@Interswitchng.com>
2019-05-28 01:24:56 +0100

    remove password field from User model json response

--- models/User.js
+++ models/User.js
@@ -2,43 +2,49 @@ const mongoose = require("mongoose");
 const Wallet = require("./Wallet");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    }, 
-    password: {
-        type: String,
-        required: true
-    }, 
-    firstName: {
-        type: String,
-        required: true
-    }, 
-    lastName: {
-        type: String,
-        required: true
-    }, 
-    email: {
-        type: String,
-        required: true
-    }, 
-    accountNumber: {
-        type: String
-    }, 
-    userType: {
-        type: String,
-        required: true
-    }, 
-    isActive: {
-        type: Boolean,
-        required: true,
-        default: true
-    }, 
-    wallet: {
-        type: Wallet
-    }
-})
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  accountNumber: {
+    type: String
+  },
+  userType: {
+    type: String,
+    required: true
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+    default: true
+  },
+  wallet: {
+    type: Wallet
+  }
+});
+
+userSchema.methods.toJSON = function() {
+  const user = this.toObject();
+  delete user.password;
+  return user;
+};
 
 const User = mongoose.model("users", userSchema);
 
